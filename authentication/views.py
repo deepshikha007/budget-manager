5fc366699136df9ffc9611d81925d13dd7f84b9f
deepshikha007 <deepshikhachauhan773@gmail.com>
2021-08-01 18:07:38 +0530

    income-expense tracker

---
+++ authentication/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
+
+# Create your views here.
